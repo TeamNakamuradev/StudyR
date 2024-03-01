bc7b93245ef643df581a59368f8cef219c0970c5
Akuldeep Jakkula <akuldeepj@gmail.com>
2024-03-01 18:26:51 +0530

    view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,35 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:http/io_client.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:studyr/main.dart';
-import 'package:studyr/utils/hex.dart';
+import 'package:http/http.dart' as http;
 
 class Dashboard extends StatefulWidget {
-  static String routeName = '/home';
-  const Dashboard({Key? key}) : super(key: key);
-
   @override
-  State<Dashboard> createState() => _DashboardState();
+  _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  final idController =
-      TextEditingController(); // Create a controller for the username
-  final pwdController =
-      TextEditingController(); // Create a controller for the password
+  List folders = [
+    {
+      'name': 'Folder 1',
+      'iconUrl':
+          'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+    },
+    {
+      'name': 'Folder 2',
+      'iconUrl':
+          'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+    },
+    {
+      'name': 'Folder 3',
+      'iconUrl':
+          'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+    },
+  ];
+  // bool isLoading = true;
+  bool isLoading = false;
+
   @override
-  void dispose() {
-    idController.dispose();
-    pwdController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    // this.fetchFolders();
   }
+
+  // fetchFolders() async {
+  //   var url = 'https://your-api-url.com/folders';
+  //   var response = await http.get(url as Uri);
+  //   if (response.statusCode == 200) {
+  //     var items = json.decode(response.body);
+  //     setState(() {
+  //       folders = items;
+  //       isLoading = false;
+  //     });
+  //   } else {
+  //     setState(() {
+  //       isLoading = false;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Folder Display'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: folders.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(folders[index]['iconUrl']),
+                    ),
+                    title: Text(
+                      folders[index]['name'],
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    tileColor: index % 2 == 0 ? Colors.white : Colors.grey[200],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
