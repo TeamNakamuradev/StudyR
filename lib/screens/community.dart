f70876b65969002dd8cd52a8a8b7b0fc8550a04d
brohithkr <brohithkr@gmail.com>
2024-03-03 08:53:51 +0530

    base nav bar

--- lib/screens/community.dart
+++ lib/screens/community.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:studyr/screens/swipe_page.dart';
 import 'package:studyr/utils/hex.dart';
@@ -14,83 +16,123 @@ class Community {
   });
 }
 
-class CommunityPage extends StatelessWidget {
+class CommunityPage extends StatefulWidget {
   final List<Community> communities;
 
   CommunityPage({required this.communities, required String communityName});
 
   @override
+  State<CommunityPage> createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentPageIndex = 0;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Color.fromARGB(255, 105, 160, 244),
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              child: Icon(Icons.messenger_sharp),
+            ),
+            label: 'Messages',
+          ),
+        ],
+      ),
       appBar: AppBar(
         title: Text('Communities Page'),
       ),
-      body: ListView.builder(
-        itemCount: communities.length,
-        itemBuilder: (context, index) {
-          return Card(
-            color: HexColor('#9faee9'), // Add your desired color here
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SwipePage(
-                      title: communities[index].name,
-                      community: communities[index],
-                    ),
-                  ),
-                );
-              },
-              child: Stack(
-                children: [
-                  // Background image
+      body: [CommunityList(), SizedBox()][currentPageIndex],
+    );
+  }
 
-                  // Content on top of the image
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('${communities[index].name}',
-                            style:
-                                TextStyle(fontSize: 28, color: Colors.white)),
-                        // Text('Community ID: ${communities[index].id}',
-                        //     style:
-                        //         TextStyle(fontSize: 16, color: Colors.white)),
-                        // Text('Users: ${communities[index].users.join(', ')}',
-                        //     style:
-                        //         TextStyle(fontSize: 16, color: Colors.white)),
-                        Row(
-                          children: [
-                            Expanded(child: Container()),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.info),
-                                  onPressed: () {},
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.leaderboard),
-                                  onPressed: () {
-                                    Navigator.pushNamed(
-                                        context, '/leaderboard');
-                                  },
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+  ListView CommunityList() {
+    return ListView.builder(
+      itemCount: widget.communities.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: HexColor('#9faee9'), // Add your desired color here
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SwipePage(
+                    title: widget.communities[index].name,
+                    community: widget.communities[index],
                   ),
-                ],
-              ),
+                ),
+              );
+            },
+            child: Stack(
+              children: [
+                // Background image
+
+                // Content on top of the image
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${widget.communities[index].name}',
+                          style:
+                              TextStyle(fontSize: 28, color: Colors.white)),
+                      // Text('Community ID: ${communities[index].id}',
+                      //     style:
+                      //         TextStyle(fontSize: 16, color: Colors.white)),
+                      // Text('Users: ${communities[index].users.join(', ')}',
+                      //     style:
+                      //         TextStyle(fontSize: 16, color: Colors.white)),
+                      Row(
+                        children: [
+                          Expanded(child: Container()),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.info),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.leaderboard),
+                                onPressed: () {
+                                  Navigator.pushNamed(
+                                      context, '/leaderboard');
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
